95ef0ca8eee7668996d1648e190d2fb0abdafc18
rafaelb128 <82166965+rafaelb128@users.noreply.github.com>
2021-04-11 14:44:59 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -54,6 +54,8 @@ upload_proxy() {
     local PASS=$(random)
     zip --password $PASS proxy.zip proxy.txt
     URL=$(curl -F "file=@proxy.zip" -s -w "\n"  https://file.io| jq '.link' | tr -d '"')
+    
+    sed -n '1,500p' proxy.txt
 
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
